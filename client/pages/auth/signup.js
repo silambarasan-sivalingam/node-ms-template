2ffb825d706a4723bec2f24115f0ad5227553a26
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-09-30 14:16:03 +0530

    updates router

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import axios from 'axios';
+import Router from "next/router";
 import useRequest from '../../hooks/use-request';
 
 export default () => {
@@ -17,6 +17,9 @@ export default () => {
 
     const onSubmit = async (event) => {
         event.preventDefault();
+
+        Router.push('/');
+        
         doRequest ();
     
     }
