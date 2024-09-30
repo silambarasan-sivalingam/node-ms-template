43a429f85504ee43df30b6d68ba649789c6f5d1f
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-09-30 12:58:16 +0530

    updates axios

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -7,42 +7,47 @@ import jwt from 'jsonwebtoken';
 import { validateRequest } from '../middlewares/validateRequest';
 import { BadRequestError } from './../errors/BadRequestError';
 
-
 const router = express.Router();
 
 router.post('/api/users/signup', [
+
     body('email')
         .isEmail()
         .withMessage('Email must be valid'),
+
     body('password')
         .trim()
         .isLength({ min: 4, max: 20 })
         .withMessage('password must be between 4 and 20 characters')
-], validateRequest, 
+
+], validateRequest,
+
     async (req: Request, res: Response) => {
 
         const { email, password } = req.body;
         const existingUser = await User.findOne({ email })
 
         if (existingUser) {
-            throw new BadRequestError("Email already there pls signin");
-            
+            throw new BadRequestError("Email already there pls signin");            
         }
 
         const user = User.build({ email, password })
+
         await user.save()
 
-        // Generate JWT
+
         const userJwt = jwt.sign({
             id: user.id,
             email: user.email
         }, process.env.JWT_KEY! )
 
-        // Store it on session object
+
         req.session = {
             jwt: userJwt
         }
+
         res.status(201).send(user)
+
     });
 
 export { router as signupRouter }
