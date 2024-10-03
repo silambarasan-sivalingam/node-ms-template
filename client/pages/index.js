2d038e0c6c90bb793b22f7c71790934f152e7c89
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-03 15:15:59 +0530

    updates code

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,22 +1,18 @@
-import buildClient from "../api/build-client";
+import buildClient from '../api/build-client';
 
-// request from component
 const LandingPage = ({ currentUser }) => {
+  // console.log(currentUser);
+  // axios.get('/api/users/currentuser');
   console.log(currentUser);
-  axios.get('/api/users/currentuser').catch((err) => {
-    console.log(err.message);
-  });
 
   return <h1>Landing Page</h1>;
 };
 
-
-// request from getInitialProps
-LandingPage.getInitialProps = async ({ context }) => {
-  const { data } = await buildClient({ context }).get("/api/users/currentuser");
+LandingPage.getInitialProps = async context => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
 
   return data;
-}
+};
 
 export default LandingPage;
-
