7442995f54e5eb64e5f0d81e3793664c70cef9dd
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-03 16:55:20 +0530

    updates init code

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,10 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import buildClient from '../api/build-client';
 
-export default ({ Component, pageProps}) => {
+const AppComponent = ({ Component, pageProps, currentUser}) => {
     return (
         <div>
-            <h1>Header!</h1>
+            <h1>Header! {currentUser.email}</h1>
             <Component {...pageProps} />
         </div>
     )
 } 
+
+AppComponent.getInitialProps = async (appContext) => {
+    console.log(appContext);
+    const client = buildClient(appContext.ctx);
+    const { data } = await client.get('/api/users/currentuser');
+    const pageProps = await appContext.Component.getInitialProps ? await appContext.Component.getInitialProps(appContext.ctx) : {};
+    console.log(pageProps);
+
+    return {
+        pageProps,
+        ...data
+    }
+}
+
+export default AppComponent;
