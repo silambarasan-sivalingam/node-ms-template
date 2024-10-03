53361b7c10370ab3942c22a0c1cea3638ad68cb3
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-03 19:14:40 +0530

    updates header

--- client/components/header.js
+++ client/components/header.js
@@ -1,6 +1,21 @@
 import Link from 'next/link';
 
 export default ({ currentUser }) => {
+    const links = [
+        !currentUser && { label: 'Sign Up', href: '/auth/signup' },
+        !currentUser && { label: 'Sign In', href: '/auth/signin' },
+        currentUser && { label: 'Sign Out', href: '/auth/signout' }
+    ]
+        .filter(linkConfig => linkConfig)
+        .map(({ label, href }) => {
+            return (
+                <li className="nav-item" key={href}>
+                    {label}
+                </li>
+            );
+        });
+
+        
     return (
         <nav className="navbar navbar-light bg-light">
             <Link className="navbar-brand" href="/">
