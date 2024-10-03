1f812ee37ff6d0bfd9c31f2ab4726a50c0c224bb
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-03 19:57:33 +0530

    updates component

--- client/components/header.js
+++ client/components/header.js
@@ -15,11 +15,11 @@ export default ({ currentUser }) => {
             );
         });
 
-        
+
     return (
         <nav className="navbar navbar-light bg-light">
             <Link className="navbar-brand" href="/">
-                GitTix
+                {label}
             </Link>
 
             <div className="d-flex justify-content-end">
