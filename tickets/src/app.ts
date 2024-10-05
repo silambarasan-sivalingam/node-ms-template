cabe1938ce36d879c710c18f59d9077ff262919a
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-05 18:50:36 +0530

    updates test

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -7,6 +7,8 @@ import cookieSession from 'cookie-session';
 import { errorHandler } from '@silambarasansivalingam/common'
 import { NotFoundError } from '@silambarasansivalingam/common'
 
+import { createTicketRouter } from './routes/new'   
+
 
 const app = express();
 app.set('trust proxy', true)
@@ -17,6 +19,8 @@ app.use(
         secure: process.env.NODE_ENV !== 'test'
     })
 )
+
+app.use(createTicketRouter)
 
 
 app.all('*', async (req, res) => {
