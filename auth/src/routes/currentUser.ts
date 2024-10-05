30ee52f507dab6810656d675306f74c50a607900
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-05 15:35:04 +0530

    updates code

--- auth/src/routes/currentUser.ts
+++ auth/src/routes/currentUser.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { currentUser } from '../middlewares/currentUser';
+import { currentUser } from '@silambarasansivalingam/common'
 
 const router = express.Router();
 
