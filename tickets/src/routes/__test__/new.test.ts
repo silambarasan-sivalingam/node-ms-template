5fdc4c3e34eb93a1fcf59cb4b2fe590c4a1dac89
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-05 18:54:21 +0530

    updates code

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -9,12 +9,12 @@ it('has a route handler listening to /api/tickets for post requests', async () =
     expect(response.status).not.toEqual(404)
 })
 
-// it('can only be accessed if the user is signed in', async () => {
-//     await request(app)
-//         .post('/api/tickets')
-//         .send({})
-//         .expect(401)
-// })
+it('can only be accessed if the user is signed in', async () => {
+    await request(app)
+        .post('/api/tickets')
+        .send({})
+        .expect(401)
+})
 
 // it('returns a status other than 401 if the user is signed in', async () => {
 //     const response = await request(app)
