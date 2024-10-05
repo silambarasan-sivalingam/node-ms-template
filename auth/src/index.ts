92ded4649e69efbf7e6ec6dd533ed8bde5ee6cd9
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-05 16:24:52 +0530

    updates code

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,8 +5,13 @@ const start = async () => {
     if (!process.env.JWT_KEY) {
         throw new Error('JWT_KEY must be defined in env');
     }
+
+    if (!process.env.MONGO_URI) {
+        throw new Error('MONGO_URI must be defined in env');
+    }
+
     try {
-        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+        await mongoose.connect(process.env.MONGO_URI)
         console.log('Connected to MongoDB')
     } catch (err) {
         console.log(err);
