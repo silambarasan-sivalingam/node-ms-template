38784404ee43cd9e8e5ee1eb7ad093a0934f7187
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-05 21:33:57 +0530

    updates code

--- tickets/src/routes/updates.ts
+++ tickets/src/routes/updates.ts
@@ -8,27 +8,29 @@ const router = express.Router();
 router.put('/api/tickets/:id', requireAuth, [
     body('title').not().isEmpty().withMessage('Title is required'),
     body('price').isFloat({ gt: 0 }).withMessage('Price must be greater than 0'),
-], validateRequest,async (req: Request, res: Response) => {
+], validateRequest, async (req: Request, res: Response) => {
 
-  const ticket = await Ticket.findById(req.params.id);
+    const ticket = await Ticket.findById(req.params.id);
 
-  if (!ticket) {
-    throw new NotFoundError();
-  }
+    if (!ticket) {
+        throw new NotFoundError();
+    }
 
-  if (ticket.userId !== req.currentUser!.id) {
-    throw new NotAuthorizedError();
-  }
+    if (ticket.userId !== req.currentUser!.id) {
+        throw new NotAuthorizedError();
+    }
 
-  ticket.set({
-    title: req.body.title,
-    price: req.body.price
-  });
+    ticket.set({
+        title: req.body.title,
+        price: req.body.price
+    });
 
-  await ticket.save();
+    await ticket.save();
 
-  res.send(ticket);
-  
-});
+    res.send(ticket);
+
+}
+
+);
 
 export { router as updateTicketRouter };
