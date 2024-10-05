707ecd13a36718d03b5f76a7e7dd88b176ec41b3
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-05 21:48:12 +0530

    updates code

--- auth/src/routes/currentUser.ts
+++ auth/src/routes/currentUser.ts
@@ -1,12 +1,10 @@
 import express from 'express';
-import { currentUser } from '@silambarasansivalingam/common'
+import { currentUser } from '@silambarasansivalingam/common';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', currentUser,(req, res) => {
-
-    res.send({ currentUser: req.currentUser || null })
-
+router.get('/api/users/currentuser', currentUser, (req, res) => {
+  res.send({ currentUser: req.currentUser || null });
 });
 
-export { router as currentUserRouter }
+export { router as currentUserRouter };
