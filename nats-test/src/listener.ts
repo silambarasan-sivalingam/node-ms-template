95cf1b550c680da6aa4f3e860afb6af9e54385ab
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-06 15:49:23 +0530

    updates nats

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -3,3 +3,7 @@ import nats from 'node-nats-streaming';
 const stan = nats.connect('ticketing', '123', {
     url: 'http://localhost:4222'
 });
+
+stan.on('connect', () => {
+    console.log('Listener Connected to NATS');
+});
