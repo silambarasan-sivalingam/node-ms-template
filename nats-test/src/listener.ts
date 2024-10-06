782f829890ea3667ee7f6c31e7464e5fbd75fdd1
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-06 17:24:20 +0530

    updates listernr

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -29,5 +29,7 @@ stan.on('connect', () => {
         if (typeof data === 'string') {
             console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
         }
+
+        msg.ack();
     });
 });
