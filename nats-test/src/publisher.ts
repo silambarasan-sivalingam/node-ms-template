86df2331ec542b563980aaec83521cc316ed220a
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-07 16:14:23 +0530

    updates publisher

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,4 +1,6 @@
 import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from './events/tickct-created-publisher';
+
 console.clear();
 const stan = nats.connect('ticketing', 'abc', {
     url: 'http://localhost:4222'
@@ -7,13 +9,24 @@ const stan = nats.connect('ticketing', 'abc', {
 stan.on('connect', () => {
     console.log('Publisher Connected to NATS');
 
-    const data = JSON.stringify({
-        id: '123',
-        title: 'concert',
-        price: 20
-    })
+    const publisher = new TicketCreatedPublisher(stan);
+    try {
+        publisher.publish({
+            id: '123',
+            title: 'concert',
+            price: 20
+        });
+    } catch (err) {
+        console.error(err);
+    }
 
-    stan.publish('ticket:created', data, () => {
-        console.log('Event Published');
-    })
+    // const data = JSON.stringify({
+    //     id: '123',
+    //     title: 'concert',
+    //     price: 20
+    // })
+
+    // stan.publish('ticket:created', data, () => {
+    //     console.log('Event Published');
+    // })
 });
