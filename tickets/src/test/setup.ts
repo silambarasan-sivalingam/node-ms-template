87f8474f295cdf887b87fa6d7cadeb6cf6534722
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-08 13:32:49 +0530

    updates setup

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -22,6 +22,7 @@ beforeAll(async () => {
 });
 
 beforeEach(async () => {
+  jest.clearAllMocks()
   if (mongoose.connection.db) {
     const collections = await mongoose.connection.db.collections();
 
