781e2fc3eef5b8ca52fa718bb9767e48f4883cb4
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-08 21:58:23 +0530

    updates tickets

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
-import { requireAuth, NotFoundError, validateRequest } from '@silambarasansivalingam/common';
+import { requireAuth, NotFoundError, validateRequest, OrderStatus, BadRequestError } from '@silambarasansivalingam/common';
 import { body } from 'express-validator';
 import { Ticket } from '../models/Ticket';
 import { Order } from '../models/Order';
@@ -19,21 +19,42 @@ router.post('/api/orders',
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    // Find the ticket the user is trying to order in the database
+
     const { ticketId } = req.body;
+
+    // Find the ticket the user is trying to order in the database
     const ticket = await Ticket.findById(ticketId);
     if (!ticket) {
       throw new NotFoundError();
     }
+
+    //Make sure that this ticket is not already reserved
+    // Run query to look at all orders. Find an order where the ticket
+    // is the ticket we just found *and* the orders status is *not* cancelled.
+    // If we find an order from that means the ticket *is* reserved
+    const existingOrder = await Order.findOne({
+        ticket: ticket,
+        status: {
+        $in: [
+          OrderStatus.Created,
+          OrderStatus.AwaitingPayment,
+          OrderStatus.Complete
+        ]
+      }
+    });
+
+    if (existingOrder) {
+      throw new BadRequestError('Ticket is already reserved');
+    }
+
+    //Calculate an expiration date for this order
+
+    //Build the order and save it to the database
+
+    //Publish an event saying that an order was created
     res.send({});
   });
 
-  //Make sure that this ticket is not already reserved
 
-  //Calculate an expiration date for this order
-
-  //Build the order and save it to the database
-
-  //Publish an event saying that an order was created
 
 export { router as newOrderRouter };
