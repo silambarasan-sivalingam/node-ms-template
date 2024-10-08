7b0e0488415492554ef695776defd735a74890f2
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-08 18:51:32 +0530

    updates news

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
 import { requireAuth, NotFoundError, validateRequest } from '@silambarasansivalingam/common';
 import { body } from 'express-validator';
 
@@ -6,9 +7,15 @@ import { body } from 'express-validator';
 const router = express.Router()
 
 router.post('/api/orders', 
+  requireAuth,
   [
-    body('ticketId').not().isEmpty().withMessage('TicketId must be provided')
+    body('ticketId')
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('TicketId must be provided')
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
     res.send({});
 });
