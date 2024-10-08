bdce8d9a55d34235b57a4d497b91001dd39afe7f
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-08 13:30:06 +0530

    updates index

--- tickets/src/__mocks__/nats-wrapper.ts
+++ tickets/src/__mocks__/nats-wrapper.ts
@@ -1,8 +1,7 @@
 export const natsWrapper = {
     client : {
-        publish: 
-            (subject: string, data: string, callback: () => void) => {
-                callback();
-            }
+        publish: jest.fn().mockImplementation((subject: string, data: string, callback: () => void) => {
+            callback();
+        })
     }
     }
--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import { app } from '../../app';
 import { Ticket } from '../../models/Ticket';
+import { natsWrapper } from '../../nats-wrapper';
 
 
 
@@ -81,4 +82,19 @@ it('creates a ticket with valid inputs', async () => {
   expect(tickets.length).toEqual(1);
   expect(tickets[0].price).toEqual(20);
   expect(tickets[0].title).toEqual(title);
+});
+
+it('publishes an event', async () => {
+  const title = 'sdfas';
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title,
+      price: 20,
+    })
+    .expect(201);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
