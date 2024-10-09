065ea10d626cfe473d5bbd9d9e7c88e459352684
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-09 20:04:29 +0530

    updates deletes

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,25 @@
 import express, { Request, Response } from 'express';
-import { NotFoundError } from '@silambarasansivalingam/common';
+import { NotFoundError, OrderStatus, NotAuthorizedError } from '@silambarasansivalingam/common';
+import { Order } from '../models/Order';
+
 
 const router = express.Router()
 
 router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId).populate('ticket');
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    // publishing an event saying this was cancelled!
     res.send({});
 });
 
