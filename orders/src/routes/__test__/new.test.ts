33f0c773ec24b9deb8164967c5d8fdf10236cc21
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-09 18:42:50 +0530

    updates test

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -19,11 +19,13 @@ const ticketId = new mongoose.Types.ObjectId();
 });
 
 it('return an error if the ticket is already reserved', async () => {
+
   const ticket = Ticket.build({
     title: 'concert',
     price: 20
   });
   await ticket.save();
+
   const order = Order.build({
     ticket,
     userId: 'sdfas',
@@ -31,6 +33,7 @@ it('return an error if the ticket is already reserved', async () => {
     expiresAt: new Date()
   });
   await order.save();
+  
   await request(app)
     .post('/api/orders')
     .set('Cookie', global.signin())
@@ -38,7 +41,7 @@ it('return an error if the ticket is already reserved', async () => {
       ticketId : ticket.id
     })
     .expect(400);
-    
+
 }); 
 
 it('reserves a ticket', async () => {
