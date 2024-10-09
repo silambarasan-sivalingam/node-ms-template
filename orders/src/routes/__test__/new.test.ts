03b89fe23ab531d53edcf612fdf5a3b55ea15f4b
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-09 18:40:39 +0530

    updates test

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import { Order } from '../../models/Order';
 import { Ticket } from '../../models/Ticket';
 import { natsWrapper } from '../../nats-wrapper';
+import { OrderStatus } from '../../models/Ticket';
 
 it('return an error if the tickets does not exist', async () => {
 const ticketId = new mongoose.Types.ObjectId();
@@ -18,7 +19,25 @@ const ticketId = new mongoose.Types.ObjectId();
 });
 
 it('return an error if the ticket is already reserved', async () => {
-
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20
+  });
+  await ticket.save();
+  const order = Order.build({
+    ticket,
+    userId: 'sdfas',
+    status: OrderStatus.Created,
+    expiresAt: new Date()
+  });
+  await order.save();
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({ 
+      ticketId : ticket.id
+    })
+    .expect(400);
 
 
 }); 
