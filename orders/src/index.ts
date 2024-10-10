750ac390e0955aeefa7bd843290c98e570b88b67
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-10 13:35:43 +0530

    updates index

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -37,6 +37,9 @@ const start = async () => {
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
 
+    new TicketCreatedListener(natsWrapper.client).listen();
+    new TicketUpdatedListener(natsWrapper.client).listen();
+
     await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDb');
     
