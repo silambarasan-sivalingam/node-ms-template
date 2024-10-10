6b9889b0dbaabdb1e4071ebc09e33a8939dce940
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-10 12:42:25 +0530

    updates node

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -69,4 +69,22 @@ it('publishes a ticket updated event', async () => {
 
 });
 
-// it.todo('emits an order created event');
+it('emits an order created event', async () => {
+  
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20
+  });
+  await ticket.save();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({ 
+      ticketId : ticket.id
+    })
+    .expect(201);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+
+});
