d007df8a180aade52e3f2a25e2c2e87fee9e67ee
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-10 12:22:09 +0530

    updates deletes

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express';
 import { NotFoundError, OrderStatus, NotAuthorizedError } from '@silambarasansivalingam/common';
 import { Order } from '../models/Order';
+import { OrderCancelledPublisher } from '../events/publishers/order-cancelled-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 
 const router = express.Router()
@@ -20,6 +22,13 @@ router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
     await order.save();
 
     // publishing an event saying this was cancelled!
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+      id: order.id,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
+
     res.send(204).send(order);
 });
 
