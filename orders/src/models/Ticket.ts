e6ecb106a62891a0c96f5ccde179d8b49cd51710
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-10 13:24:56 +0530

    updates tickets

--- orders/src/models/Ticket.ts
+++ orders/src/models/Ticket.ts
@@ -5,6 +5,7 @@ import { OrderStatus } from '@silambarasansivalingam/common';
 export { OrderStatus }
 
 interface TicketAttrs {
+  id: string;
   title: string;
   price: number;
 }
@@ -42,8 +43,14 @@ const ticketSchema = new mongoose.Schema(
 );
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
-  return new Ticket(attrs);
-};
+  return new Ticket(
+    {
+      _id: attrs.id,
+      title: attrs.title,
+      price: attrs.price,
+    })
+}
+
 ticketSchema.methods.isReserved = async function () {
   // this === the ticket document that we just called 'isReserved' on
   const existingOrder = await Order.findOne({
