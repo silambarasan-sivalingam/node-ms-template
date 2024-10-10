d7b34dd9394be2366d4dc30e5d5a419bc52d34a8
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-10 11:53:51 +0530

    updates orders cancelled

--- common/src/events/order-created-event.ts
+++ common/src/events/order-created-event.ts
@@ -1,15 +1,16 @@
 import { Subjects } from './subjects';
+import { OrderStatus } from './types/order-status';
 
 export interface OrderCreatedEvent {
     subject: Subjects.OrderCreated;
     data: {
         id: string;
-        status: string;
+        status: OrderStatus;
         userId: string;
         expiresAt: string;
         ticket: {
-        id: string;
-        price: number;
+            id: string;
+            price: number;
         };
     };
-    }
+}
