634d1d5c228cf8276767a8f0e8671b73198599af
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-10 11:55:19 +0530

    updates modules

--- common/build/events/subjects.js
+++ common/build/events/subjects.js
@@ -5,4 +5,7 @@ var Subjects;
 (function (Subjects) {
     Subjects["TicketCreated"] = "ticket:created";
     Subjects["TicketUpdated"] = "ticket:updated";
+    Subjects["OrderCreated"] = "order:created";
+    Subjects["OrderCancelled"] = "order:cancelled";
+    Subjects["PaymentCreated"] = "payment:created";
 })(Subjects || (exports.Subjects = Subjects = {}));
