c616f0b16f227319d13e87f3b3431b33ba4a08e8
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-10 13:29:37 +0530

    updates tickets

---
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -0,0 +1,23 @@
+import { Message } from 'node-nats-streaming';
+import { Subjects, Listener, TicketUpdatedEvent } from '@silambarasansivalingam/common';
+import { Ticket } from '../../models/Ticket';
+import { queueGroupName } from './queue-group-name';
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+  queueGroupName = queueGroupName ;
+
+  async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
+        const ticket = await Ticket.findById(data.id)
+
+        if (!ticket) {
+            throw new Error('ticket not found')
+        }
+
+        const { title, price } = data
+        ticket.set({ title, price })
+        await ticket.save()
+
+        msg.ack()
+  }
+}
