323a7e4b9419e205cc8df68b03ed7a3bca1a7266
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-10 16:53:37 +0530

    updates tickets

--- tickets/src/models/Ticket.ts
+++ tickets/src/models/Ticket.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
 
 interface TicketAttrs {
 
@@ -12,8 +13,9 @@ interface TicketDoc extends mongoose.Document {
     title: string;
     price: number;
     userId: string;
-
+    version: number;
 }
+
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
 
@@ -43,6 +45,9 @@ const ticketSchema = new mongoose.Schema({
         }
     }
 })
+ticketSchema.set('versionKey', 'version');
+ticketSchema.plugin(updateIfCurrentPlugin);
+
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
     return new Ticket(attrs)
