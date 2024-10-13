bcd2fc39e0ef935d44f940cc15e8d5adc5efd428
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-13 21:57:36 +0530

    updates order cancelled events

--- tickets/src/events/listeners/order-cancelled-listener.ts
+++ tickets/src/events/listeners/order-cancelled-listener.ts
@@ -1,11 +1,22 @@
 import { Listener, OrderCancelledEvent, Subjects } from "@silambarasansivalingam/common";
 import { queueGroupName } from "./queue-group-name";
 import { Message } from "node-nats-streaming";
+import { Ticket } from "../../models/Ticket";
 
 export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
     subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
     queueGroupName = queueGroupName;
-    onMessage(data: OrderCancelledEvent['data'], msg: Message) {
+
+    async onMessage(data: OrderCancelledEvent['data'], msg: Message) {
+        const ticket = await Ticket.findById(data.ticket.id);
+
+        if (!ticket) {
+            throw new Error('Ticket not found');
+        }
+
+        ticket.set({ orderId: undefined });
+        await ticket.save();
+
         msg.ack();
     }
 }
