ee7a084570f4163d9c7fcb4f2b581677d9b4a7e3
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-13 20:27:17 +0530

    updates orders

--- orders/src/models/Order.ts
+++ orders/src/models/Order.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 import { TicketDoc } from './Ticket';
 import { OrderStatus } from '@silambarasansivalingam/common';
 
@@ -51,6 +52,9 @@ const orderSchema = new mongoose.Schema(
     },
   }
 );
+
+orderSchema.set('versionKey', 'version');
+orderSchema.plugin(updateIfCurrentPlugin);
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
   return new Order(attrs);
