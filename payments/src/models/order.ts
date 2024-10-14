d6b473fc7be08bdbb8b7028c6182294aa00febbf
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-14 18:00:54 +0530

    updates orders

--- payments/src/models/order.ts
+++ payments/src/models/order.ts
@@ -1,5 +1,6 @@
 import { OrderStatus } from "@silambarasansivalingam/common";
 import mongoose from "mongoose";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
 
 
 // list of properties that are required to building the order 
@@ -27,8 +28,42 @@ interface OrderModel extends mongoose.Model<OrderDoc> {
 
 // creating the order schema
 const orderSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: Object.values(OrderStatus),
+    }
+}, {
+        toJSON: {
+            transform(doc, ret) {
+                ret.id = ret._id;
+                delete ret._id;
+            }
+        }
+    }
+);
 
-});
+orderSchema.set('versionKey', 'version');
+orderSchema.plugin(updateIfCurrentPlugin);
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+    return new Order({
+        _id: attrs.id,
+        version: attrs.version,
+        userId: attrs.userId,
+        price: attrs.price,
+        status: attrs.status
+    });
+}
+
 
 // creating the order model
 const Order = mongoose.model<OrderDoc, OrderModel>('Order', orderSchema);
