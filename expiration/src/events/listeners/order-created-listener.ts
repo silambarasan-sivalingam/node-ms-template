ea35abb138dbb0efde87a57a9fd19a02f0e8bc00
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-14 13:34:44 +0530

    updates packages

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,15 @@
+
+import { Listener, OrderCreatedEvent, Subjects } from '@silambarasansivalingam/common';
+import { queueGroupName } from './queue-group-name';
+import { Message } from 'node-nats-streaming';
+
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+    subject: Subjects.OrderCreated = Subjects.OrderCreated;
+    queueGroupName = queueGroupName;
+
+    async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+        // ...
+    }
+
+}
