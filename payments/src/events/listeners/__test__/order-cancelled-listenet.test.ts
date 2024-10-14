10617034661ce0d53ef191aab1c11b77241afdbf
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-14 18:47:41 +0530

    updates order listner

--- payments/src/events/listeners/__test__/order-cancelled-listenet.test.ts
+++ payments/src/events/listeners/__test__/order-cancelled-listenet.test.ts
@@ -6,31 +6,47 @@ import { natsWrapper } from "../../../nats-wrapper";
 
 
 const setup = async () => {
+  const listener = new OrderCancelledListener(natsWrapper.client);
 
-    const listener = new OrderCancelledListener(natsWrapper.client);
+  const order = Order.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    status: OrderStatus.Created,
+    price: 10,
+    userId: 'asldkfj',
+    version: 0,
+  });
+  await order.save();
 
-    const order = Order.build({
-        id: new mongoose.Types.ObjectId().toHexString(),
-        version: 0,
-        price: 10,
-        userId: '123',
-        status: OrderStatus.Created
-    });
+  const data: OrderCancelledEvent['data'] = {
+    id: order.id,
+    version: 1,
+    ticket: {
+      id: 'asldkfj',
+    },
+  };
 
-    await order.save();
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
 
-    const data: OrderCancelledEvent['data'] = {
-        id: order.id,
-        version: 1,
-        ticket: {
-            id: '123'
-        }
-    };
+  return { listener, data, msg, order };
+};
 
-    //@ts-ignore
-    const msg: Message = {
-        ack: jest.fn()
-    };
+it('updates the status of the order', async () => {
+  const { listener, data, msg, order } = await setup();
 
-    return { listener, order, data, msg };
-}
+  await listener.onMessage(data, msg);
+
+  const updatedOrder = await Order.findById(order.id);
+
+  expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});
+
+it('acks the message', async () => {
+  const { listener, data, msg, order } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  expect(msg.ack).toHaveBeenCalled();
+});
--- payments/src/events/listeners/order-cancelled-listener.ts
+++ payments/src/events/listeners/order-cancelled-listener.ts
@@ -5,28 +5,26 @@ import { Order } from "../../models/order";
 import { idText } from "typescript";
 
 export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
-
     subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
     queueGroupName = queueGroupName;
-
+  
     async onMessage(data: OrderCancelledEvent['data'], msg: Message) {
-        const order = await Order.findOne({
-            id: data.id,
-            version: data.version - 1
-        });
-
-        if (!order) {
-            throw new Error('Order not found');
-        }
-
-        order.set({ status: OrderStatus.Cancelled });
-        await order.save();
-        
-
-        msg.ack();
-}
-}
-
+      const order = await Order.findOne({
+        _id: data.id,
+        version: data.version - 1,
+      });
+  
+      if (!order) {
+        throw new Error('Order not found');
+      }
+  
+      order.set({ status: OrderStatus.Cancelled });
+      await order.save();
+  
+      msg.ack();
+    }
+  }
+  
 
 // onMessage(data: { id: string; version: number; status: OrderStatus; userId: string; expiresAt: string; ticket: { id: string; price: number; }; }, msg: Message): void {
 //     throw new Error("Method not implemented.");
