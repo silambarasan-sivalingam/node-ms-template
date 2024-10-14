dad344400949bbed564851e466a9d6050e93c884
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-14 15:29:23 +0530

    updates expiration complete events

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -1,6 +1,15 @@
-import { Listener } from "@silambarasansivalingam/common";
+import { Listener, Subjects, ExpirationCompleteEvent } from "@silambarasansivalingam/common";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queue-group-name";
 
-export class ExpirationCompleteListener extends Listener {
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
 
+    subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+    queueGroupName = queueGroupName;
+
+    async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
+        console.log('Expiration complete event received for orderId:', data.orderId);
+        msg.ack();
+    }
 }
 
