8952af9f3b46becfa78ce4e4b01fefc82b1fe20c
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-14 14:51:57 +0530

    updates packages

--- common/src/events/subjects.ts
+++ common/src/events/subjects.ts
@@ -4,4 +4,7 @@ export enum Subjects {
 
   OrderCreated = 'order:created',
   OrderCancelled = 'order:cancelled',
+
+  ExpirationComplete = 'expiration:complete',
+  PaymentCreated = 'payment:created',
 }
