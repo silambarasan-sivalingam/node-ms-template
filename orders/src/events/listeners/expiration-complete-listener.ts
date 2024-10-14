361226304ee001b67f3eca331999a08d65ed2805
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-14 15:31:18 +0530

    updates expiration date

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -1,6 +1,7 @@
 import { Listener, Subjects, ExpirationCompleteEvent } from "@silambarasansivalingam/common";
 import { Message } from "node-nats-streaming";
 import { queueGroupName } from "./queue-group-name";
+import { Order } from "../../models/Order";
 
 export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
 
@@ -8,6 +9,19 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
     queueGroupName = queueGroupName;
 
     async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
+
+        const order = await Order.findById(data.orderId).populate('ticket');
+
+        if (!order) {
+            throw new Error('Order not found');
+        }
+
+        order.set({
+            status: 'cancelled',
+        });
+        
+        await order.save();
+
         console.log('Expiration complete event received for orderId:', data.orderId);
         msg.ack();
     }
