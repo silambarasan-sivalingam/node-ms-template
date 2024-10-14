e812af2ff133e3de6e833c46acc2d38f7537e86a
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-14 14:28:43 +0530

    updates orders

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -2,14 +2,23 @@
 import { Listener, OrderCreatedEvent, Subjects } from '@silambarasansivalingam/common';
 import { queueGroupName } from './queue-group-name';
 import { Message } from 'node-nats-streaming';
-
+import { expirtationQueue } from '../../queues/expiration-queue';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     subject: Subjects.OrderCreated = Subjects.OrderCreated;
     queueGroupName = queueGroupName;
 
     async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
-        // ...
+        const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+        console.log('Waiting this many milliseconds to process the job:', delay);
+
+        await expirtationQueue.add({
+            orderId: data.id
+        }, {
+            delay
+        });
+
+        msg.ack();
     }
 
 }
