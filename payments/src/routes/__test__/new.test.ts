f4799926a03b50ded2041132fa6f45155d129d61
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-15 12:02:34 +0530

    updates new test

---
+++ payments/src/routes/__test__/new.test.ts
@@ -0,0 +1,19 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+
+it('returns a 404 when purchasing an order that does not exist', async () => {
+
+});
+
+
+it('returns a 401 when purchasing an order that does not belong to the user', async () => {
+
+});
+
+
+it('returns a 400 when purchasing a cancelled order', async () => {
+  
+});
+
+
