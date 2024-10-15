c4e84b4810e4a39daa51ff9d27cfa2602ba08e6e
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-15 19:22:48 +0530

    updates payments

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -3,6 +3,10 @@ import { app } from '../../app';
 import mongoose from 'mongoose';
 import { Order } from '../../models/order';
 import { OrderStatus } from '@silambarasansivalingam/common';
+import { stripe } from '../../stripe';
+import { Payment } from '../../models/payment';
+
+jest.mock('../../stripe');
 
 
 it('returns a 404 when purchasing an order that does not exist', async () => {
@@ -62,4 +66,32 @@ it('returns a 400 when purchasing a cancelled order', async () => {
   
 });
 
+
+// it('returns a 204 with valid inputs', async () => {
+
+//     const userId = new mongoose.Types.ObjectId().toHexString();
+
+//     const order = Order.build({
+//         id: new mongoose.Types.ObjectId().toHexString(),
+//         userId,
+//         version: 0,
+//         price: 20,
+//         status: OrderStatus.Created
+//     });
+//     await order.save();
+
+//     await request(app)
+//         .post('/api/payments')
+//         .set('Cookie', global.signin(userId))
+//         .send({
+//             token: 'tok_visa',
+//             orderId: order.id
+//         })
+//         . expect(201)
+
+//         const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0];
+//         expect(chargeOptions.source).toEqual('tok_visa');
+//         expect(chargeOptions.amount).toEqual(20 * 100);
+//         expect(chargeOptions.currency).toEqual('usd');
+// });
 
--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -2,6 +2,7 @@ import { body } from 'express-validator';
 import exppress, { Request, Response } from 'express';
 import { requireAuth, validateRequest, BadRequestError, NotFoundError, NotAuthorizedError, OrderStatus } from '@silambarasansivalingam/common';
 import { Order} from '../models/order';
+import { Payment } from '../models/payment';
 import { stripe } from '../stripe';
 
 
@@ -28,13 +29,21 @@ router.post('/api/payments', requireAuth, [
     throw new BadRequestError('Cannot pay for an cancelled order');
   }
 
-  await stripe.charges.create({
+  const charge = await stripe.charges.create({
     currency: 'usd',
     amount: order.price * 100,
     source: token
   });
 
-  res.send({ success: true });
+  const payment = Payment.build({
+    orderId,
+    stripeId: charge.id
+  });
+
+  await payment.save();
+
+
+  res.status(201).send({ success: true });
 });
 
 export { router as createChargeRouter };
