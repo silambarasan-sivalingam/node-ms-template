019d818d5377b5d35d94e7e141bbb499d6088353
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-15 15:40:49 +0530

    updates stripe

--- payments/src/stripe.ts
+++ payments/src/stripe.ts
@@ -1,9 +1,9 @@
 import Stripe from 'stripe';
 
-if (!process.env.STRIPE_SECRET_KEY) {
+if (!process.env.STRIPE_KEY) {
     throw new Error('Stripe Secret Key is missing from environment variables');
   }
   
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+export const stripe = new Stripe(process.env.STRIPE_KEY!, {
   apiVersion: '2024-09-30.acacia'
 });
