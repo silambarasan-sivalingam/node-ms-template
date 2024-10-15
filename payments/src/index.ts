5ccb0eb413ca0227e851463a8bf615743243d2e3
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-15 10:18:55 +0530

    updates index.js

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
-
 import { app } from './app';
-
 import { natsWrapper } from './nats-wrapper';
+import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
+import { OrderCreatedListener } from './events/listeners/order-created-listener';
 
 
 const start = async () => {
@@ -28,7 +28,7 @@ const start = async () => {
 
   try {
     await natsWrapper.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID, process.env.NATS_URL);
-    
+
     natsWrapper.client.on('close', () => {
       console.log('NATS connection closed!');
       process.exit();
@@ -37,9 +37,12 @@ const start = async () => {
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
 
+    new OrderCreatedListener(natsWrapper.client).listen();
+    new OrderCancelledListener(natsWrapper.client).listen();
+
     await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDb');
-    
+
   } catch (err) {
     console.error(err);
   }
