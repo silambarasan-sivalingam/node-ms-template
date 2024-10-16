112051d78b829cb712886fde1fb496cefef2c256
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-16 11:15:26 +0530

    updates payment created listener

--- orders/src/events/listeners/payment-created-listener.ts
+++ orders/src/events/listeners/payment-created-listener.ts
@@ -1,13 +1,25 @@
-import { Subjects, Listener, PaymentCreatedEvent } from  '@silambarasansivalingam/common';
+import { Subjects, Listener, PaymentCreatedEvent, OrderStatus } from  '@silambarasansivalingam/common';
 import { Message } from 'node-nats-streaming';
+import { queueGroupName } from './queue-group-name';
+import { Order } from '../../models/Order';
 
 
 export class PaymentCreatedListener extends Listener<PaymentCreatedEvent> {
   subject: Subjects.PaymentCreated = Subjects.PaymentCreated;
   queueGroupName = 'payments-service';
 
-  onMessage(data: PaymentCreatedEvent['data'], msg: Message) {
-    console.log('PaymentCreatedEvent received', data);
+  async onMessage(data: PaymentCreatedEvent['data'], msg: Message) {
+    const order = await Order.findById(data.orderId);
+
+    if (!order) {
+      throw new Error('Order not found');
+    }
+
+    order.set({
+      status: OrderStatus.Complete
+    });
+
+    await order.save();
 
     msg.ack();
   }
--- orders/src/index.ts
+++ orders/src/index.ts
@@ -3,6 +3,7 @@ import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
 import { TicketCreatedListener } from './events/listeners/ticket-created-listener';
 import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener';
+import { PaymentCreatedListener } from './events/listeners/payment-created-listener';
 
 
 const start = async () => {
@@ -39,6 +40,7 @@ const start = async () => {
 
     new TicketCreatedListener(natsWrapper.client).listen();
     new TicketUpdatedListener(natsWrapper.client).listen();
+    new PaymentCreatedListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDb');
