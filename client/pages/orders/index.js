8b5139934191b58beee2b5793f6542fb2c3fa39a
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-17 08:45:31 +0530

    updates orders status

--- client/pages/orders/index.js
+++ client/pages/orders/index.js
@@ -2,7 +2,7 @@ const OrderIndex = ({ orders}) => {
     return (
         <ul>
             {orders.map(order => {
-                return <li key={order.id}>{order.status}</li>
+                return <li key={order.id}>{order.ticket.title} - {order.status}</li>
             })}
         </ul>
     )
