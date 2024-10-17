c540229bbbb16448c5c2374b62e79995732d3327
silambarasan-sivalingam <silambarasan.sivalingam@outlook.com>
2024-10-17 09:20:02 +0530

    updates code

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -11,7 +11,7 @@ beforeAll(async () => {
 });
 
 beforeEach(async () => {
-  const collections = await mongoose.connection.db.collections();
+  const collections = await mongoose.connection.db?.collections() || [];
 
   for (let collection of collections) {
     await collection.deleteMany({});
